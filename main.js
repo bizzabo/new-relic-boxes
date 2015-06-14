336d2c363fd107f42d8bf9b7949b2e7912c3775c
Tal Bereznitskey <berzniz@gmail.com>
2015-06-14 11:18:35 +0300

    Sane updated interval (20 seconds)

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function MainCtrl($scope, $resource, poller, localStorageService) {
     $scope.config = {
         grouping: [{type: 'prod', rank: 1, alarm: true}, {type: 'demo', rank: 2, alarm: true}, {type: 'int', rank: 3}, {type: 'dev', rank: 4}],
         defaultGroup: {type: 'general', rank: 1000},
-        defaultPollingTime: 20 * 1000 * 1000
+        defaultPollingTime: 20 * 1000
     };
 
     $scope.healthCheckRank = {
